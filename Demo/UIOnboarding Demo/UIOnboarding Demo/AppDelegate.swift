5be7ca28e35b318ec89c492bd7b8290a695a9b2a
Luke <luuk.scc@outlook.de>
2022-02-17 20:23:02 +0100

    Added a demo project with SPM.

---
+++ Demo/UIOnboarding Demo/UIOnboarding Demo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  UIOnboarding Demo
+//
+//  Created by Lukman Aščić on 17.02.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return .init(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+}
---
+++ Demo/UIOnboarding Demo/UIOnboarding Demo/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  UIOnboarding Demo
+//
+//  Created by Lukman Aščić on 17.02.22.
+//
+
+import UIKit
+import SwiftUI
+import UIOnboarding
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+        presentOnboarding()
+    }
+            
+    @objc private func presentOnboarding() {
+        let onboardingController: UIOnboardingViewController = .init(withConfiguration: .setUp())
+        onboardingController.delegate = self
+        navigationController?.present(onboardingController, animated: false)
+    }
+}
+
+extension ViewController {
+    private func setUp() {
+        view.backgroundColor = .systemBackground
+        
+        navigationController?.navigationBar.tintColor = .init(named: "camou")
+        navigationItem.rightBarButtonItem = .init(image: .init(systemName: "repeat"), style: .plain, target: self, action: #selector(presentOnboarding))
+    }
+}
+
+extension ViewController: UIOnboardingViewControllerDelegate {
+    func didFinishOnboarding(onboardingViewController: UIOnboardingViewController) {
+        onboardingViewController.modalTransitionStyle = .crossDissolve
+        onboardingViewController.dismiss(animated: true, completion: nil)
+    }
+}
+
+#if DEBUG
+struct ViewControllerContainer: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UINavigationController {
+        return .init(rootViewController: ViewController.init())
+    }
+    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {}
+}
+
+struct ContentViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ViewControllerContainer()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+#endif
